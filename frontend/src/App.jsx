8a8aeb66f1814514fbf0ed4f89ddefa02d3a00b8
Vedant Patole <patolevedant11@gmail.com>
2026-05-28 22:33:13 +0530

    fixed failing import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import axios from "axios";
-import { Button } from "@/components/ui/Button";
+import { Button } from "./components/ui/Button";
 
 function App() {
   const [database, setDatabase] = useState("");
